89b350bd2e7cba6c0ce4531171f4c5f5704a98c3
willianctti <willianctti@gmail.com>
2023-11-23 20:47:37 -0300

    finalizing modal

--- Senhas/App.js
+++ Senhas/App.js
@@ -1,7 +1,26 @@
-import { View, Text, StyleSheet } from "react-native"
+import { useState } from "react"
+import { View, Text, StyleSheet, Image, TouchableOpacity, Modal } from "react-native"
 import Slider from '@react-native-community/slider'
+import ModalPassword from "./components/modal"
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$"
 
 function App() {
+  const [size, setSize] = useState(10)
+  const [passwordValue, setPasswordValue] = useState('')
+  const [modalVisible, setModalVisible] = useState(false)
+
+  function generatePassword() {
+    let password = "";
+    for(let i = 0, n = charset.length; i < size; i++) {
+      password += charset.charAt(Math.floor(Math.random() * n))
+    }
+
+    setPasswordValue(password)
+    setModalVisible(true)
+  }
+
+
   return (
     <View style={styles.container}>
       <Image
@@ -9,7 +28,7 @@ function App() {
         style={styles.logo}
       />
 
-    <Text style={styles.title}>20 Caracteres</Text>      
+    <Text style={styles.title}>{size} Caracteres</Text>      
 
     <View style={styles.area}>
     <Slider
@@ -18,8 +37,19 @@ function App() {
     maximumValue={20}
     maximumTrackTintColor="#ff0000"
     minimumTrackTintColor="#000"
+    value={size}
+    onValueChange={ (value)=> setSize(value.toFixed(0)) }
     />
     </View>
+
+    <TouchableOpacity onPress={generatePassword} style={styles.button}>
+      <Text style={styles.buttonText}>Gerar senha</Text>
+    </TouchableOpacity>
+
+    <Modal visible={modalVisible} animationType="fade" transparent={true}>
+      <ModalPassword password={passwordValue} handleClose={() => setModalVisible(false)}/>
+
+    </Modal>
 
     </View>
 
@@ -43,6 +73,24 @@ function App() {
       backgroundColor: '#FFF',
       borderRadius: 8,
       padding: 9
+    },
+    button: {
+      backgroundColor: "purple",
+      width: '80%',
+      height: 50,
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: 8,
+    },
+    buttonText: {
+      color: '#FFF',
+      fontSize: 20,
+      marginBottom: 18,
+      marginTop: 10
+    },
+    title: {
+      fontSize: 20,
+      fontWeight: 'bold'
     }
   })
 
